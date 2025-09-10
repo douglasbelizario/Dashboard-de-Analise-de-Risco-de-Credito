10d1a142733f96b798ee816d6fad3c166b93badb
Douglas Belizario <doug0423@hotmail.com>
2025-09-10 20:47:39 -0300

    Versão inicial do projeto estruturado

---
+++ README.md
@@ -0,0 +1,42 @@
+# Dashboard de Análise de Risco de Crédito
+
+Este projeto é um Dashboard interativo construído com Streamlit para analisar dados de risco de crédito de clientes.
+
+## 📜 Descrição
+
+O dashboard permite a visualização de clientes por faixa etária, risco, e outras métricas importantes. Os dados foram tratados e analisados no notebook que se encontra na pasta `/notebooks`.
+
+## 🚀 Como Executar o Projeto Localmente
+
+1. Clone o repositório:
+   ```bash
+   git clone [https://github.com/seu-usuario/seu-repositorio.git](https://github.com/seu-usuario/seu-repositorio.git)
+   ```
+2. Navegue até a pasta do projeto:
+   ```bash
+   cd meu_dashboard_risco
+   ```
+3. Ative o ambiente virtual (Se necessário):
+   ```bash
+   # Windows
+   .\.venv\Scripts\activate
+   # macOS/Linux
+   source .venv/bin/activate
+   ```
+4. Instale as dependências:
+   ```bash
+   pip install -r requirements.txt
+   ```
+5. Rode o aplicativo Streamlit:
+   ```bash
+   streamlit run app.py
+   ```
+
+## 📂 Estrutura do Projeto
+- **/notebooks/Dashboard_credito_risco.ipynb**: Notebook com toda a análise exploratória e o processo de limpeza e tratamento dos dados.
+- **/dados/**: Contém os datasets brutos e tratados.
+- **app.py**: O script principal que executa a aplicação Streamlit.
+- **requirements.txt**: Lista de dependências do projeto.
+
+## 🔗 Link para o App Online
+*(...)*
---
+++ app.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Dashboard de Risco de Crédito",
+    page_icon="🪙",
+    layout="wide"
+)
+
+@st.cache_data
+def carregar_dados():
+    df = pd.read_csv('credito_alemao_tratado_final.csv', sep=';', decimal=',')
+    return df
+
+df = carregar_dados()
+
+st.sidebar.header("Filtros")
+
+proposito_selecionado = st.sidebar.selectbox(
+    "Selecione o Propósito do Crédito",
+    options=df['Proposito'].unique(),
+    index=None, # Faz com que a opção 'placeholder' apareça
+    placeholder="Todos"
+)
+
+faixa_etaria_selecionada = st.sidebar.selectbox(
+    "Selecione a Faixa Etária",
+    options=df['Faixa_Etaria'].unique(),
+    index=None,
+    placeholder="Todos"
+)
+
+# Aplicando os filtros ao DataFrame
+if proposito_selecionado:
+    df = df[df['Proposito'] == proposito_selecionado]
+
+if faixa_etaria_selecionada:
+    df = df[df['Faixa_Etaria'] == faixa_etaria_selecionada]  
+
+# Aplicando os filtros ao DataFrame
+if proposito_selecionado:
+    df = df[df['Proposito'] == proposito_selecionado]
+
+if faixa_etaria_selecionada:
+    df = df[df['Faixa_Etaria'] == faixa_etaria_selecionada]
+
+
+st.title("🪙 Dashboard de Análise de Risco de Crédito")
+st.markdown("---")
+
+total_clientes = df.shape[0]
+maus_pagadores = df[df['Risco'] == 'Ruim'].shape[0]
+taxa_inadimplencia = (maus_pagadores / total_clientes) * 100 if total_clientes > 0 else 0
+valor_total_credito = df['Valor_Credito'].sum()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Total de Clientes", f"{total_clientes:,}".replace(",", "."))
+with col2:
+    st.metric("Taxa de Inadimplência", f"{taxa_inadimplencia:.2f}%")
+with col3:
+    st.metric("Valor Total Emprestado (DM)", f"{valor_total_credito:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+
+st.markdown("---")
+
+col_graf1, col_graf2, col_graf3 = st.columns(3)
+
+with col_graf1:
+    # Gráfico1: Risco por Propósito do Crédito
+    st.subheader("Risco por Propósito do Crédito")
+    fig_proposito = px.histogram(df, 
+                                 x='Proposito', 
+                                 color='Risco', 
+                                 barmode='group',
+                                 text_auto=True,
+                                 color_discrete_map={'Bom': '#1f77b4', 'Ruim': '#d62728'})
+    st.plotly_chart(fig_proposito, use_container_width=True)
+
+with col_graf2:
+    # Gráfico2: Proporção de Risco (Pizza)
+    st.subheader("Proporção Geral de Risco")
+    df_risco = df['Risco'].value_counts().reset_index()
+    fig_risco = px.pie(df_risco, 
+                       names='Risco', 
+                       values='count',
+                       color='Risco',
+                       color_discrete_map={'Bom': '#1f77b4', 'Ruim': '#d62728'},
+                       labels={
+                                        "Proposito": "Propósito do cliente",
+                                        "count": "Quantidade de Clientes",
+                                        "Risco": "Risco de Crédito"
+                                    }
+                        )
+                    
+
+    st.plotly_chart(fig_risco, use_container_width=True)
+
+with col_graf3:    
+    # Gráfico3: Total de clientes por faixa etária
+    st.subheader("Risco por Faixa Etária")
+    fig_faixa_etaria = px.histogram(df,
+                              x='Faixa_Etaria',
+                              color="Risco",
+                              barmode='group',
+                              text_auto=True,
+                              color_discrete_map={'Bom': '#1f77b4', 'Ruim': '#d62728'},
+                              labels={
+                                        "Faixa_Etaria": "Faixa Etária",
+                                        "count": "Quantidade de Clientes",
+                                        "Risco": "Risco de Crédito"   
+                                    }
+                              ) 
+    st.plotly_chart(fig_faixa_etaria, use_container_width=True)                          
+
+
